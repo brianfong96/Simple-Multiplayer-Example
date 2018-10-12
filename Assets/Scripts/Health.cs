aa553a7a427a27496f9fd44c62abb08f3cef8673
Brian Fong <brianwkfong@gmail.com>
2018-10-11 17:21:51 -0700

    health networking works

--- Assets/Scripts/Health.cs
+++ Assets/Scripts/Health.cs
@@ -1,20 +1,32 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.Networking;
 
-public class Health : MonoBehaviour {
+public class Health : NetworkBehaviour {
 
-    public const int MAX_HEALTH = 100;
-    public int CurrentHealth = MAX_HEALTH;
+
+    public const int MAX_HEALTH = 100;    
     public RectTransform HealthBar;
+
+    [SyncVar(hook ="OnChangeHealth")]
+    public int CurrentHealth = MAX_HEALTH;
 
     public void TakeDamage(int amount)
     {
+        if (!isServer) return;
+
         CurrentHealth -= amount;
         if (CurrentHealth <= 0)
         {
             CurrentHealth = 0;
             Debug.Log("Dead!");
         }
-        HealthBar.sizeDelta = new Vector2(CurrentHealth, HealthBar.sizeDelta.y);
+        
     }
+
+    void OnChangeHealth(int health)
+    {
+        HealthBar.sizeDelta = new Vector2(health, HealthBar.sizeDelta.y);
+    }
+
 }
