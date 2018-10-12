dd0162c6a67d4cb27a58242cb1894f88002966ab
Brian Fong <brianwkfong@gmail.com>
2018-10-11 19:08:24 -0700

    added in spawn points

--- Assets/Scripts/Health.cs
+++ Assets/Scripts/Health.cs
@@ -4,6 +4,7 @@ using UnityEngine.Networking;
 
 public class Health : NetworkBehaviour {
 
+    private NetworkStartPosition[] spawnPoints;
 
     public const int MAX_HEALTH = 100;    
     public RectTransform HealthBar;
@@ -12,6 +13,14 @@ public class Health : NetworkBehaviour {
     //[SyncVar(hook ="OnChangeHealth")]
     [SyncVar]
     public int CurrentHealth = MAX_HEALTH;
+
+    private void Start()
+    {
+        if (isLocalPlayer)
+        {
+            spawnPoints = FindObjectsOfType<NetworkStartPosition>();
+        }
+    }
 
     public void TakeDamage(int amount)
     {
@@ -37,8 +46,17 @@ public class Health : NetworkBehaviour {
     {
         if (isLocalPlayer)
         {
-            // move back to zero location
-            transform.position = Vector3.zero;
+            // Set the spawn point to origin as a default value
+            Vector3 spawnPoint = Vector3.zero;
+
+            // If there is a spawn point array and the array is not empty, pick a spawn point at random
+            if (spawnPoints != null && spawnPoints.Length > 0)
+            {
+                spawnPoint = spawnPoints[Random.Range(0, spawnPoints.Length)].transform.position;
+            }
+
+            // Set the player’s position to the chosen spawn point
+            transform.position = spawnPoint;
         }
     }
     
